7c933860350cf7708b7bb968d4d81a11bfa9cd5f
Dare <ogundipe.obadare@gmail.com>
2018-04-25 14:43:52 -0400

    added click handling

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
+
 var script = () => {
   var button = document.getElementById("bluesquare");
+  button.addEventListener("click", () => {
+    button.classList.add("moveme");
+  });
 };
 
 document.addEventListener("DOMContentLoaded", script);
-
-// debugger
